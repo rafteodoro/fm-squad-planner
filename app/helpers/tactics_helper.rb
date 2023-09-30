34d3fb944f0202608a8af09aac5af591dd6ce86f
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-09-30 18:22:29 -0400

    change position attributes to match player

--- app/helpers/tactics_helper.rb
+++ app/helpers/tactics_helper.rb
@@ -3,10 +3,19 @@ module TacticsHelper
   def get_best_player(position, players)
     # Goes through all players
     players.each do |player|
+      sum = 0
       # Finds the position map for the player
       posmap = Posmap.find_by(player_id: player.id)
-      # Compares the position map with the given position
-      
+      # Goes through all posmaps attributes to check the position rating
+      posmap.attributes.each do |key, value|
+        sum += (value * position.position_w) if key == position.name
+      end
+      # If the position attribute is of type float and it is not 0 then multiply it by the player attribute with the same name
+      position.attributes.each do |key, value|
+        sum += (value * player.attributes[key]) if key == position.name && value.instance_of?(Float) && value.positive?
+      end
+
+
     end
   end
 
