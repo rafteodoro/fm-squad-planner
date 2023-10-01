7ac99ed5df5de10ecd1fc392e971505af8a94817
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-09-30 20:06:16 -0400

    create evaluate function and show the three best in the view

--- app/helpers/tactics_helper.rb
+++ app/helpers/tactics_helper.rb
@@ -1,6 +1,11 @@
+# Frozen_string_literal: true
+
+# This helper is used to calculate the best players for a given position
 module TacticsHelper
   # Returns the best player for a given position
-  def get_best_player(position, players)
+  def get_best_players(position, players)
+    rated_players = []
+    positionsum = positionsum(position)
     # Goes through all players
     players.each do |player|
       sum = 0
@@ -10,14 +15,31 @@ module TacticsHelper
       posmap.attributes.each do |key, value|
         sum += (value * position.position_w) if key == position.name
       end
-      # If the position attribute is of type float and it is not 0 then multiply it by the player attribute with the same name
+      # If the position attribute is of type float and it is not 0 then multiply it by the player same name attribute
       position.attributes.each do |key, value|
-        sum += (value * player.attributes[key]) if key == position.name && value.instance_of?(Float) && value.positive?
+        if value.instance_of?(Float) && value.positive?
+          value = 21 - value if %w[h_injury_proneness h_dirtiness eccentricity].any? { |s| key.include?(s) }
+          sum += (value * player.attributes[key]) unless player.attributes[key].nil?
+        end
       end
-
-
+      # Calculates the rating for the player and make it a percentage with only two decimals
+      rating = ((sum / positionsum) * 100).round(1)
+      rated_players << [player, rating]
     end
+    # Sorts the rated players by rating
+    rated_players.sort_by! { |_, rating| rating }.reverse!
+    # Returns the sorted array
+    rated_players
   end
 
+  private
 
+  # Calculates the maximum sum for a given position
+  def positionsum(position)
+    sum = 60
+    position.attributes.each do |_, value|
+      sum += (value * 20) if value.instance_of?(Float) && value.positive?
+    end
+    sum
+  end
 end
