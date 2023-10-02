caa470db9566e10135382b5b103790746108eebb
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-10-02 16:02:04 -0400

    fix layout and order of role and mentality

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+# Helper methods for the Players controller.
 module PlayersHelper
+  # Retrieve the player posmap
+  def player_posmap(player)
+    string = ''
+    posmap = Posmap.find_by(player_id: player.id)
+    playable_positions(posmap).each do |position|
+      string += position + ' '
+    end
+    string
+  end
 end
