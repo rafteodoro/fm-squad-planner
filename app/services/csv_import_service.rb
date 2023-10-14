29bf944407b083f71e9e3b7d00e0fd6436238548
Rafael Teodoro <rafaelteodoro@rocketmail.com>
2023-10-13 22:05:18 -0400

    modify import csv to recognize the columns from the file

--- app/services/csv_import_service.rb
+++ app/services/csv_import_service.rb
@@ -21,12 +21,17 @@ class CsvImportService
       player_hash = addGoalkeeperFields(player_hash, row)
 
       if player_hash[:name].nil? == false
-        Player.find_or_create_by!(player_hash)
+        player = Player.find_or_initialize_by(uid: player_hash[:uid])
+
         # Create a posmap for the player created
-        player = Player.find_by(uid: player_hash[:uid])
-        posmap = Posmap.new
-        posmap.player_id = player.id
-        posmap.save
+        debugger
+        if player.new_record?
+          posmap = Posmap.new
+          posmap.player_id = player.id
+          posmap.save
+        end
+
+        player.update(player_hash)
       end
 
       # for performance, you could create a separate job to import each user
@@ -37,68 +42,68 @@ class CsvImportService
   def addTechnicalFields(player_hash, row)
     player_hash[:foot_right] = row['Right Foot']
     player_hash[:foot_left] = row['Left Foot']
-    player_hash[:corners] = row['Corners'].to_i
-    player_hash[:crossing] = row['Crossing'].to_i
-    player_hash[:dribbling] = row['Dribbling'].to_i
-    player_hash[:finishing] = row['Finishing'].to_i
-    player_hash[:first_touch] = row['First Touch'].to_i
-    player_hash[:free_kicks] = row['Free Kicks'].to_i
-    player_hash[:heading] = row['Heading'].to_i
-    player_hash[:long_shots] = row['Long Shots'].to_i
-    player_hash[:long_throw] = row['Long Throw'].to_i
-    player_hash[:marking] = row['Marking'].to_i
-    player_hash[:passing] = row['Passing'].to_i
-    player_hash[:penalty_taking] = row['Penalty Taking'].to_i
-    player_hash[:tackling] = row['Tackling'].to_i
-    player_hash[:technique] = row['Technique'].to_i
+    player_hash[:corners] = row['Cor'].to_i
+    player_hash[:crossing] = row['Cro'].to_i
+    player_hash[:dribbling] = row['Dri'].to_i
+    player_hash[:finishing] = row['Fin'].to_i
+    player_hash[:first_touch] = row['Fir'].to_i
+    player_hash[:free_kicks] = row['Fre'].to_i
+    player_hash[:heading] = row['Hea'].to_i
+    player_hash[:long_shots] = row['Lon'].to_i
+    player_hash[:long_throw] = row['L Th'].to_i
+    player_hash[:marking] = row['Mar'].to_i
+    player_hash[:passing] = row['Pas'].to_i
+    player_hash[:penalty_taking] = row['Pen'].to_i
+    player_hash[:tackling] = row['Tck'].to_i
+    player_hash[:technique] = row['Tec'].to_i
 
     player_hash
   end
 
   def addMentalFields(player_hash, row)
-    player_hash[:aggression] = row['Aggression'].to_i
-    player_hash[:anticipation] = row['Anticipation'].to_i
-    player_hash[:bravery] = row['Bravery'].to_i
-    player_hash[:composure] = row['Composure'].to_i
-    player_hash[:concentration] = row['Concentration'].to_i
-    player_hash[:decisions] = row['Decisions'].to_i
-    player_hash[:determination] = row['Determination'].to_i
-    player_hash[:flair] = row['Flair'].to_i
-    player_hash[:leadership] = row['Leadership'].to_i
-    player_hash[:off_the_ball] = row['Off the Ball'].to_i
-    player_hash[:positioning] = row['Positioning'].to_i
-    player_hash[:teamwork] = row['Teamwork'].to_i
-    player_hash[:vision] = row['Vision'].to_i
-    player_hash[:work_rate] = row['Work Rate'].to_i
+    player_hash[:aggression] = row['Agg'].to_i
+    player_hash[:anticipation] = row['Ant'].to_i
+    player_hash[:bravery] = row['Bra'].to_i
+    player_hash[:composure] = row['Cmp'].to_i
+    player_hash[:concentration] = row['Cnt'].to_i
+    player_hash[:decisions] = row['Dec'].to_i
+    player_hash[:determination] = row['Det'].to_i
+    player_hash[:flair] = row['Fla'].to_i
+    player_hash[:leadership] = row['Ldr'].to_i
+    player_hash[:off_the_ball] = row['OtB'].to_i
+    player_hash[:positioning] = row['Pos'].to_i
+    player_hash[:teamwork] = row['Tea'].to_i
+    player_hash[:vision] = row['Vis'].to_i
+    player_hash[:work_rate] = row['Wor'].to_i
 
     player_hash
   end
 
   def addPhysicalFields(player_hash, row)
-    player_hash[:acceleration] = row['Acceleration'].to_i
-    player_hash[:agility] = row['Agility'].to_i
-    player_hash[:balance] = row['Balance'].to_i
-    player_hash[:jumping] = row['Jumping'].to_i
-    player_hash[:natural_fitness] = row['Natural Fitness'].to_i
-    player_hash[:pace] = row['Pace'].to_i
-    player_hash[:stamina] = row['Stamina'].to_i
-    player_hash[:strength] = row['Strength'].to_i
+    player_hash[:acceleration] = row['Acc'].to_i
+    player_hash[:agility] = row['Agi'].to_i
+    player_hash[:balance] = row['Bal'].to_i
+    player_hash[:jumping] = row['Jum'].to_i
+    player_hash[:natural_fitness] = row['Nat'].to_i
+    player_hash[:pace] = row['Pac'].to_i
+    player_hash[:stamina] = row['Sta'].to_i
+    player_hash[:strength] = row['Str'].to_i
 
     player_hash
   end
 
   def addGoalkeeperFields(player_hash, row)
-    player_hash[:aerial_reach] = row['Aerial Reach'].to_i
-    player_hash[:command_of_area] = row['Command Of Area'].to_i
-    player_hash[:communication] = row['Communication'].to_i
-    player_hash[:eccentricity] = row['Eccentricity'].to_i
-    player_hash[:handling] = row['Handling'].to_i
-    player_hash[:kicking] = row['Kicking'].to_i
-    player_hash[:one_on_ones] = row['One On Ones'].to_i
-    player_hash[:punching] = row['Punching'].to_i
-    player_hash[:reflexes] = row['Reflexes'].to_i
-    player_hash[:rushing_out] = row['Rushing Out'].to_i
-    player_hash[:throwing] = row['Throwing'].to_i
+    player_hash[:aerial_reach] = row['Aer'].to_i
+    player_hash[:command_of_area] = row['Cmd'].to_i
+    player_hash[:communication] = row['Com'].to_i
+    player_hash[:eccentricity] = row['Ecc'].to_i
+    player_hash[:handling] = row['Han'].to_i
+    player_hash[:kicking] = row['Kic'].to_i
+    player_hash[:one_on_ones] = row['1v1'].to_i
+    player_hash[:punching] = row['Pun'].to_i
+    player_hash[:reflexes] = row['Ref'].to_i
+    player_hash[:rushing_out] = row['TRO'].to_i
+    player_hash[:throwing] = row['Thr'].to_i
 
     player_hash
   end
